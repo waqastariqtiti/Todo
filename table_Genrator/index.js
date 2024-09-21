67229eea39613c142eacc169813a74f68862e178
waqas <tariqwaqasttii@gmail.com>
2024-09-21 16:29:29 +0500

    final change submited

--- table_Genrator/index.js
+++ table_Genrator/index.js
@@ -1,19 +1,16 @@
 const inpu = document.querySelector(".input");
 const res = document.querySelector(".result");
 
-
-function genrate(){
+function genrate() {
     res.innerHTML = "";
-    if (inpu.value === "") {
-        alert("Enter a number to Genrate any Table")
+    if (inpu.value === "" || isNaN(inpu.value)) {
+        res.innerHTML = "<p style='color:red;'>Please enter a valid number</p>";
         return;
-    }else{
-        for (let i = 1; i <= 10; i++) {
-            res.innerHTML += `${inpu.value} * ${i} = ${i*inpu.value} <br>`;
-            console.log(i); 
-        }
-        inpu.value = "";
-        inpu.focus();
     }
-
+    for (let i = 1; i <= 10; i++) {
+        res.innerHTML += `${inpu.value} * ${i} = ${i * inpu.value} <br>`;
+        console.log(i);
+    }
+    inpu.value = "";
+    inpu.focus();
 }
